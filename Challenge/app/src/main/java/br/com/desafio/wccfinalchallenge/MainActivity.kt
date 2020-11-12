6f8e6397429c5ec9fbd795bed04a5c7050626bb2
Livia Nakashima <43746512+LiviaNakashima@users.noreply.github.com>
2020-11-12 00:43:19 -0300

    Add MainActivity and ProductsActivity with string resource

--- Challenge/app/src/main/java/br/com/desafio/wccfinalchallenge/MainActivity.kt
+++ Challenge/app/src/main/java/br/com/desafio/wccfinalchallenge/MainActivity.kt
@@ -1,11 +1,20 @@
 package br.com.desafio.wccfinalchallenge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun loginButton(component: View) {
+
+        val login = Intent(this, Products::class.java)
+
+        startActivity(login)
     }
 }
---
+++ Challenge/app/src/main/java/br/com/desafio/wccfinalchallenge/Products.kt
@@ -0,0 +1,11 @@
+package br.com.desafio.wccfinalchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Products : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_products)
+    }
+}
